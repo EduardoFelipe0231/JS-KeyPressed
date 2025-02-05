64f5577e15dc09bdfbdc1c4c7eb6ec0965083596
EduardoFelipe0231 <edufelipe.232@hotmail.com>
2025-02-05 01:50:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const resultC = document.getElementById('resultC');
 const resultB = document.getElementById('resultB');
 let reload = document.getElementById('reloadFull');
 
+const keyPressedSound = new Audio();
+keyPressedSound.src = "key.mp3"
  
 
 window.addEventListener("keydown", e => {
@@ -14,18 +16,22 @@ window.addEventListener("keydown", e => {
         resultC.innerHTML = `Você digitou a tecla <span>${e.key.toLocaleUpperCase()}</span>`
         document.body.style.backgroundColor = '#E630C3';
         resultC.style.color = 'black';
+        keyPressedSound.play();
 
     } else if (e.key === keyB){
 
         resultC.innerHTML = `Você digitou a tecla <span>${e.key.toLocaleUpperCase()}</span>`
         document.body.style.backgroundColor = '#4630E6';
         resultC.style.color = 'white';
+        keyPressedSound.play();
 
     } else{
 
         resultC.innerHTML = `Você digitou a tecla <span>${e.key.toLocaleUpperCase()}</span>`
         document.body.style.backgroundColor = '#F9FAFF';
         resultC.style.color = 'black';
+        keyPressedSound.play();
+        
     }
  
 
